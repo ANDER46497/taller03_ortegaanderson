c62f10a19a7a33aaec145a36133e75b1677f7cb7
Anderson ortega <andersonortega030cuc.edu.co>
2024-09-23 10:22:13 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import miLogo from "./assets/img/futbol.jpg";
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={miLogo} alt="Logo Nuevo" />
+        <h1>¡¡¡Hola Mundo!!!</h1>
+        <p>Este es mi primer proyecto en React...</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
